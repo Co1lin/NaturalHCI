7c79d3c3fe622ce76c04c39bd54b17ec364f495a
Achazwl <323163497@qq.com>
2021-12-21 12:44:39 +0800

    fix piano play mode

--- Assets/GestureOne.cs
+++ Assets/GestureOne.cs
@@ -2,6 +2,9 @@ using Microsoft.MixedReality.Toolkit.Utilities;
 
 public class GestureOne : GestureWidget
 {
+    public override void Start() {
+        _audioSource.enabled = false;
+    }
     public override bool GestureCondition()
     {
         return IsOne(_handedness_right);
